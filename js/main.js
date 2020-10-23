1f0a9aa9ef82449fc2d90b66804d0dba43a19bea
“Haabdeldjalil <abdeldjalilhachimi12@gmail.com>
2020-10-23 13:38:47 +0100

    home page modification

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 $(document).ready(function () {
-  // button
-  $("#btn").on("click", function () {
-    console.log("button clicked");
+  // Scroll to first element
+  $("#lead-down").click(function () {
+    var scrollDistance = $("#lead").next().offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: scrollDistance + "px",
+      },
+      500
+    );
+  });
+  // Scroll to top
+  $("#to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
+
+  // Counter up
+  $(".counter").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
   });
 });
